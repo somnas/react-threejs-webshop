7b2c2cf097f2862c76d1137ec50bc8794a0888af
Erik Somnäs <erik.somnas@gmail.com>
2023-04-24 10:57:38 +0200

    start implementing animation components in home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
 
+import { motion, AnimatePresence } from 'framer-motion';
+import { useSnapshot } from 'valtio';
+
+import state from '../store';
+
+import {
+    headContainerAnimation,
+    headContentAnimation,
+    headTextAnimation,
+    slideAnimation
+} from '../config/motion';
+
 export default function Home() {
+
+    const snap = useSnapshot(state);
+
     return (
-        <div>Home</div>
+        <AnimatePresence>
+            {snap.intro && (
+                <motion.div className='home' {...slideAnimation('left')}>
+                    <motion.header {...slideAnimation('down')}>
+                        <img
+                            src='./threejs.png'
+                            alt='logo'
+                            className='w-8 h-8 object-contain'
+                        />
+                    </motion.header>
+                </motion.div>
+            )}
+        </AnimatePresence>
     );
 }
